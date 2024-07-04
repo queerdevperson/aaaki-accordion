1b8d12c4f2f45c1776254a4f708f10266222a46a
lefty55104 <143383087+queerdevperson@users.noreply.github.com>
2024-07-04 02:26:53 -0400

    Use the ACF built-in way to include the script

--- functions.php
+++ functions.php
@@ -8,20 +8,3 @@ function ow_init() {
 	register_block_type( __DIR__ . '/blocks/ow-accordion-panel' );
 }
 add_action( 'init', 'ow_init' );
-
-/**
- * Enqueue files
- */
-function ow_styles() {
-	
-	if ( has_block('acf/ow-accordion') ) {
-		wp_register_script( 
-			'ow-accordion-block', 
-			get_template_directory_uri() . '/blocks/ow-accordion/block.js', 
-			array('acf')
-		);
-
-		wp_enqueue_script('ow-accordion-block');
-	}
-}
-add_action( 'wp_enqueue_scripts', 'ow_styles' );
